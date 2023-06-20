c3f9683de3d7ab2df4412543bdc70ee13017fbe4
Nestor Vega <vega.nestor1@gmail.com>
2023-06-20 11:40:29 -0700

    We are able to obtain all items from dynamoDB

--- backend/api/app/bonus_checker.py
+++ backend/api/app/bonus_checker.py
@@ -1,14 +1,20 @@
 import boto3
 import botocore
 from datetime import datetime, timedelta
-from .config import TABLE_NAME, AWS_SERVER_PUBLIC_KEY, AWS_SERVER_SECRET_KEY
+# from .config import TABLE_NAME, AWS_SERVER_PUBLIC_KEY, AWS_SERVER_SECRET_KEY
 import json
 
 with open('CHECKER_DATA.json', 'r') as file:
     banks = json.load(file)
 
-dynamodb = boto3.resource("dynamodb", region_name='us-west-1',aws_access_key_id=AWS_SERVER_PUBLIC_KEY, aws_secret_access_key=AWS_SERVER_SECRET_KEY)
+# dynamodb = boto3.resource("dynamodb", region_name='us-west-1',aws_access_key_id=AWS_SERVER_PUBLIC_KEY, aws_secret_access_key=AWS_SERVER_SECRET_KEY)
+# table = dynamodb.Table(TABLE_NAME)
+TABLE_NAME = 'aws-plaid-demo-app-Table-1XR3R6NLWD0B6'
+AWS_ACCESS_KEY_ID = 'AKIAQ76F6KJL7CHXEM4Q'
+AWS_SECRET_ACCESS_KEY = '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'
+dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table(TABLE_NAME)
+dynamodb_client = dynamodb.meta.client
 
 class UserBankAccountCheck:
     DATA_FORMAT = "%Y-%m-%d"
@@ -128,14 +134,23 @@ class FormatAccountData:
 
 # Fetch all data from DynamoDB
 def fetch_transactions_data():
+    # response = table.scan()
+    # return response.get("Items")
+    data = []
     response = table.scan()
-    return response.get("Items")
+    data.extend(response.get("Items"))
 
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response.get("Items"))
+
+    return data
 
 # Main function to call to identify accounts
 def bonus_checker():
     try:
         all_transactions_data = fetch_transactions_data()
+        print(f"Total number of items in the table: {len(all_transactions_data)}")
 
         for bank, bank_conditions in banks.items():
             for bank_condition in bank_conditions:
