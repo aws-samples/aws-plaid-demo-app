e47baa738a76fd3b7ff6f3ea8a364355bc05f22d
ericdoppelt <ead45@duke.edu>
2023-07-05 15:15:04 -0400

    Fixed protected.js

--- frontend/src/pages/Protected.js
+++ frontend/src/pages/Protected.js
@@ -7,32 +7,10 @@ import Institutions from '../components/Institutions';
 const logger = new Logger("Protected");
 
 export default function Protected() {
-  const [items, setItems] = useState([]);
-
-  const getItems = async () => {
-    try {
-      const res = await API.graphql(graphqlOperation(GetItems));
-      logger.info(res);
-      setItems(res.data.getItems.items);
-    } catch (err) {
-      logger.error('unable to get items', err);
-    }
-  }
-
-  useEffect(() => {
-    getItems();
-  }, []);
 
   return (
     <Flex direction="column">
       <Plaid/>
-      {(items && items.length) ? (
-        <View>
-          <Heading>Institutions</Heading>
-          <Institutions institutions={items}/>
-        </View>
-      ) : (<div/>)
-      }
     </Flex>
   );
 }
