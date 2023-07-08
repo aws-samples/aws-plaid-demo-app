9ea010f57b1c55e4745a4e723ebd5476fc2b745f
Jordan <jordan@caseswift.io>
2023-07-08 13:59:32 -0400

    Updated email sender to parse actual plaid data

--- backend/api/app/send.py
+++ backend/api/app/send.py
@@ -12,8 +12,67 @@ from reportlab.pdfbase.pdfmetrics import stringWidth
 
 client = boto3.client('ses',region_name="us-east-2")
 
+def createPdf(pdf, width, height, item):
+    # employer info
+    employerName = item.payroll_income[0].pay_stubs[0].employer.name
+    employerStreet = item.payroll_income[0].pay_stubs[0].employer.address.street
+    employerTownAndZip = item.payroll_income[0].pay_stubs[0].employer.address.city + ", " + item.payroll_income[0].pay_stubs[0].employer.address.region + " " + item.payroll_income[0].pay_stubs[0].employer.address.postal_code
 
-def format_plaid_income(pdf, width, height, employerName, employerStreet, employerTownAndZip, employerPhone,
+    # employee info
+    employeeName = item.payroll_income[0].pay_stubs[0].employee.name
+    employeeStreet = item.payroll_income[0].pay_stubs[0].employee.address.street
+    employeeTownAndZip = item.payroll_income[0].pay_stubs[0].employee.address.city + ", " + item.payroll_income[0].pay_stubs[0].employee.address.region + " " + item.payroll_income[0].pay_stubs[0].employee.address.postal_code
+
+    # pay period info
+    payPeriodStart = item.payroll_income[0].pay_stubs[0].pay_period_details.start_date
+    payPeriodEnd = item.payroll_income[0].pay_stubs[0].pay_period_details.end_date
+    payDate = item.payroll_income[0].pay_stubs[0].pay_period_details.pay_date
+
+    # payment summary info 
+    grossEarnings = item.payroll_income[0].pay_stubs[0].pay_period_details.gross_earnings
+    totalTaxesAndDeductions = item.payroll_income[0].pay_stubs[0].deductions.total.current_amount
+    netPay = item.payroll_income[0].pay_stubs[0].net_pay.current_amount
+    bankName = item.payroll_income[0].pay_stubs[0].pay_period_details.distribution_breakdown[0].bank_name
+    bankMask = item.payroll_income[0].pay_stubs[0].pay_period_details.distribution_breakdown[0].mask
+
+    # gross earnings info
+    currentSalary = 0
+    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
+        if breakdown.canonical_description == "REGULAR_PAY":
+            currentSalary = breakdown.current_amount
+    ytdSalary = 0
+    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
+        if breakdown.canonical_description == "REGULAR_PAY":
+            currentSalary = breakdown.ytd_amount
+    currentSupplementalBonus = 0
+    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
+        if breakdown.canonical_description == "BONUS":
+            currentSalary = breakdown.current_amount
+    ytdSupplementalBonus = 0
+    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
+        if breakdown.canonical_description == "BONUS":
+            currentSalary = breakdown.ytd_amount
+    currentTotalEarnings = currentSalary + currentSupplementalBonus
+    ytdTotalEarnings = ytdSalary + ytdSupplementalBonus
+
+    # taxes withheld info
+    currentFederalIncomeTax = item.payroll_income[0].pay_stubs[0].federal_income_tax_withheld
+    currentSocialSecurity = item.payroll_income[0].pay_stubs[0].social_security_wages
+    currentMedicare = item.payroll_income[0].pay_stubs[0].medicare_tax_withheld
+    currentStateTax = item.payroll_income[0].pay_stubs[0].state_and_local_wages.state_income_tax
+    currentTotalTaxWithheld = item.payroll_income[0].pay_stubs[0].deductions.total.current_amount
+    ytdTotalTaxWithheld = item.payroll_income[0].pay_stubs[0].deductions.total.ytd_amount
+
+    format_plaid_income(pdf, width, height, employerName, employerStreet, employerTownAndZip,
+                        employeeName, employeeStreet, employeeTownAndZip,
+                        payPeriodStart, payPeriodEnd, payDate,
+                        str(grossEarnings), str(totalTaxesAndDeductions), str(netPay), bankName, str(bankMask),
+                        str(currentSalary), str(ytdSalary), str(currentSupplementalBonus), str(ytdSupplementalBonus), str(currentTotalEarnings), str(ytdTotalEarnings),
+                        str(currentFederalIncomeTax), str(currentSocialSecurity), str(currentMedicare), str(currentStateTax), str(currentTotalTaxWithheld), str(ytdTotalTaxWithheld))
+    pdf.showPage()
+    pdf.save()
+
+def format_plaid_income(pdf, width, height, employerName, employerStreet, employerTownAndZip,
                         employeeName, employeeStreet, employeeTownAndZip,
                         payPeriodStart, payPeriodEnd, payDate,
                         grossEarnings, totalTaxesAndDeductions, netPay, bankName, bankMask,
@@ -35,7 +94,6 @@ def format_plaid_income(pdf, width, height, employerName, employerStreet, employ
     pdf.drawString((inch*1.1), height-(1.4*inch), employerName)
     pdf.drawString((inch*1.1), height-(1.6*inch), employerStreet)
     pdf.drawString((inch*1.1), height-(1.8*inch), employerTownAndZip)
-    pdf.drawString((inch*1.1), height-(2*inch), employerPhone)
 
     # Draw Employee Information
     pdf.setFont("Helvetica-Bold", 10)
@@ -177,49 +235,45 @@ def format_plaid_income(pdf, width, height, employerName, employerStreet, employ
 
     return pdf   
     
-msg = MIMEMultipart()
-msg['Subject'] = 'Your Requested Documents from CaseSwift'
-msg['From'] = 'jordan@caseswift.io'
-msg['To'] = 'jordan@caseswift.io'
 
-# what a recipient sees if they don't use an email reader
-msg.preamble = 'CaseSwift Documents.\n'
+def send_email(sender, recipient, plaidResponseObject):
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Your Requested Documents from CaseSwift'
+    msg['From'] = sender
+    msg['To'] = recipient
 
-# the message body
-part = MIMEText('Howdy -- here are your requested Documents')
-msg.attach(part)
+    # what a recipient sees if they don't use an email reader
+    msg.preamble = 'CaseSwift Documents.\n'
 
-plaidFinancialInformationPdf = canvas.Canvas("plaid_income_verification.pdf", pagesize=letter)
-width, height = letter
-format_plaid_income(plaidFinancialInformationPdf, width, height, "CaseSwift LLC", "1309 Hill Street", "Durham, NC 27707", "(123) 456-7890",
-                    "Jordan Castleman", "445 W 35th Street", "New York, New York 10001",
-                    "2020-12-15", "2021-12-15", "2021-6-15",
-                    "$123,456", "$123,456", "$123,456", "Bank of America", "1234",
-                    "123,456", "$123,456", "123", "$0", "$123,456", "$123,456",
-                    "$123,456", "$123,456", "$123,456", "$123,456", "$123,456", "$123,456")
-plaidFinancialInformationPdf.showPage()
-plaidFinancialInformationPdf.save()
+    # the message body
+    part = MIMEText('Howdy -- here are your requested Documents')
+    msg.attach(part)
 
-# the attachment
-part = MIMEApplication(open('plaid_income_verification.pdf', 'rb').read())
-part.add_header('Content-Disposition', 'attachment', filename='plaid_income_verification.pdf')
-msg.attach(part)
+    counter = 1
+    for item in plaidResponseObject.items:
+        pdfName = "plaid_income_verification" + str(counter) + ".pdf"
+        plaidFinancialInformationPdf = canvas.Canvas(pdfName, pagesize=letter)
+        width, height = letter
+        createPdf(plaidFinancialInformationPdf, width, height, item)
+        part = MIMEApplication(open(pdfName, 'rb').read())
+        part.add_header('Content-Disposition', 'attachment', filename=pdfName)
+        msg.attach(part)
+        counter+=1
 
+    # and send the message
+    try: 
 
-# and send the message
-try: 
+        response = client.send_raw_email(
+            Source=msg['From'],
+            Destinations=[msg['To']],
+            RawMessage={
+            'Data': msg.as_string()
+        },)
 
-    response = client.send_raw_email(
-        Source=msg['From'],
-        Destinations=[msg['To']],
-        RawMessage={
-        'Data': msg.as_string()
-    },)
-
-except ClientError as e:
-    print(e.response['Error']['Message'])
-else:
-    print("Email sent! Message ID:"),
-    print(response['MessageId'])
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
 
 
