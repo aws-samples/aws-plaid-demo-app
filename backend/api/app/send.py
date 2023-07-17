cbe77de9009855afc40e5cb34e93e7a5ee0cc96f
Jordan <jordan@caseswift.io>
2023-07-17 11:25:32 -0400

    Reworked send.py

--- backend/api/app/send.py
+++ backend/api/app/send.py
@@ -12,7 +12,7 @@ from reportlab.pdfbase.pdfmetrics import stringWidth
 
 client = boto3.client('ses',region_name="us-east-2")
 
-def createPdf(pdf, width, height, item):
+def createPlaidPayrollPdf(pdf, width, height, item):
     # employer info
     employerName = item.payroll_income[0].pay_stubs[0].employer.name
     employerStreet = item.payroll_income[0].pay_stubs[0].employer.address.street
@@ -37,21 +37,26 @@ def createPdf(pdf, width, height, item):
 
     # gross earnings info
     currentSalary = 0
+    ytdSalary = 0
+    currentSupplementalBonus = 0
+    ytdSupplementalBonus = 0
+    currentOvertime = 0
+    ytdOvertime = 0
+    currentCommission = 0
+    ytdCommission = 0
     for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
         if breakdown.canonical_description == "REGULAR_PAY":
             currentSalary = breakdown.current_amount
-    ytdSalary = 0
-    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
-        if breakdown.canonical_description == "REGULAR_PAY":
-            ytdSalary = breakdown.ytd_amount
-    currentSupplementalBonus = 0
-    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
+            ytdSalary = breakdown.ytd_amount            
         if breakdown.canonical_description == "BONUS":
             currentSupplementalBonus = breakdown.current_amount
-    ytdSupplementalBonus = 0
-    for breakdown in item.payroll_income[0].pay_stubs[0].earnings.breakdown:
-        if breakdown.canonical_description == "BONUS":
-            ytdSupplementalBonus = breakdown.ytd_amount
+            ytdSupplementalBonus = breakdown.ytd_amount 
+        if breakdown.canonical_description == "OVERTIME":
+            currentOvertime = breakdown.current_amount
+            ytdOvertime = breakdown.ytd_amount
+        if breakdown.canonical_description == "COMMISSION":
+            currentCommission = breakdown.current_amount
+            ytdCommission = breakdown.ytd_amount
     currentTotalEarnings = currentSalary + currentSupplementalBonus
     ytdTotalEarnings = ytdSalary + ytdSupplementalBonus
 
@@ -67,7 +72,7 @@ def createPdf(pdf, width, height, item):
                         employeeName, employeeStreet, employeeTownAndZip,
                         str(payPeriodStart), str(payPeriodEnd), str(payDate),
                         str(grossEarnings), str(totalTaxesAndDeductions), str(netPay), bankName, str(bankMask),
-                        str(currentSalary), str(ytdSalary), str(currentSupplementalBonus), str(ytdSupplementalBonus), str(currentTotalEarnings), str(ytdTotalEarnings),
+                        str(currentSalary), str(ytdSalary), str(currentSupplementalBonus), str(ytdSupplementalBonus), str(currentOvertime), str(ytdOvertime), str(currentCommission), str(ytdCommission), str(currentTotalEarnings), str(ytdTotalEarnings),
                         str(currentFederalIncomeTax), str(currentSocialSecurity), str(currentMedicare), str(currentStateTax), str(currentTotalTaxWithheld), str(ytdTotalTaxWithheld))
     pdf.showPage()
     pdf.save()
@@ -76,158 +81,201 @@ def format_plaid_income(pdf, width, height, employerName, employerStreet, employ
                         employeeName, employeeStreet, employeeTownAndZip,
                         payPeriodStart, payPeriodEnd, payDate,
                         grossEarnings, totalTaxesAndDeductions, netPay, bankName, bankMask,
-                        currentSalary, ytdSalary, currentSupplementalBonus, ytdSupplementalBonus, currentTotalEarnings, ytdTotalEarnings,
+                        currentSalary, ytdSalary, currentSupplementalBonus, ytdSupplementalBonus, currentOvertime, ytdOvertime, currentCommission, ytdCommission, currentTotalEarnings, ytdTotalEarnings,
                         currentFederalIncomeTax, currentSocialSecurity, currentMedicare, currentStateTax, currentTotalTaxWithheld, ytdTotalTaxWithheld):
+    
+    heightTracker = 0.75
+    
     # Add CaseSwift Header
     headerString = "CaseSwift Income Verification Statement"
     pdf.setFont("Helvetica-Bold", 10)
     pdf.setFillColor(navy)
     headerStringWidth = stringWidth(headerString, "Helvetica-Bold", 10)
-    pdf.drawString((width - headerStringWidth)/2, height-(0.75*inch), headerString)
-    pdf.line((width/2)-(headerStringWidth/2), height-(inch*0.85), (width/2)+(headerStringWidth/2), height-(inch*0.85))
+    pdf.drawString((width - headerStringWidth)/2, height-(heightTracker*inch), headerString)
+    heightTracker += 0.1
+    pdf.line((width/2)-(headerStringWidth/2), height-(inch*heightTracker), (width/2)+(headerStringWidth/2), height-(inch*heightTracker))
 
-    # Draw Employer Information
+    # Draw Employer and Employee Information
     pdf.setFillColor(black)
     pdf.setFont("Helvetica-Bold", 10)
+    heightTracker += 0.35
     pdf.drawString((inch*1.1), height-(1.2*inch), "Employer:")
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), "Employee:")
     pdf.setFont("Helvetica", 10)
-    pdf.drawString((inch*1.1), height-(1.4*inch), employerName)
-    pdf.drawString((inch*1.1), height-(1.6*inch), employerStreet)
-    pdf.drawString((inch*1.1), height-(1.8*inch), employerTownAndZip)
-
-    # Draw Employee Information
-    pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawRightString(width-(inch*1.1), height-(1.2*inch), "Employee:")
-    pdf.setFont("Helvetica", 10)
-    pdf.drawRightString(width-(inch*1.1), height-(1.4*inch), employeeName)
-    pdf.drawRightString(width-(inch*1.1), height-(1.6*inch), employeeStreet)
-    pdf.drawRightString(width-(inch*1.1), height-(1.8*inch), employeeTownAndZip)
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), employerName)
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), employeeName)
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), employerStreet)
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), employeeStreet)
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), employerTownAndZip)
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), employeeTownAndZip)
 
     # Box Employer/Employee
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.rect(0.9*inch, height-(2.1*inch), width-(1.8*inch), inch*1.1)
+    pdf.rect(0.9*inch, height-(heightTracker*inch), width-(1.8*inch), inch*1.1)
 
     # Draw Pay Period Information
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(2.5*inch), "Pay Period Start: ")
+    heightTracker += 0.7
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Pay Period Start: ")
     pdf.setFont("Helvetica", 10)
-    pdf.drawRightString(width-(inch*1.1), height-(2.5*inch), payPeriodStart)
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), payPeriodStart)
 
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(2.7*inch), "Pay Period End: ")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Pay Period End: ")
     pdf.setFont("Helvetica", 10)
-    pdf.drawRightString(width-(inch*1.1), height-(2.7*inch), payPeriodEnd)
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), payPeriodEnd)
 
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(2.9*inch), "Pay Date: ")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Pay Date: ")
     pdf.setFont("Helvetica", 10)
-    pdf.drawRightString(width-(inch*1.1), height-(2.9*inch), payDate)
+    pdf.drawRightString(width-(inch*1.1), height-(heightTracker*inch), payDate)
 
     #Box Pay Period Information
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.rect(0.9*inch, height-(3*inch), (width-(1.8*inch)), inch*0.7)
+    heightTracker += 0.1
+    pdf.rect(0.9*inch, height-(heightTracker*inch), (width-(1.8*inch)), inch*0.7)
 
     # Draw Payment Summary Information
     pdf.setFont("Helvetica-Bold", 10)
+    heightTracker += 0.2
     paymentSummaryStringWidth = stringWidth("Payment Summary", "Helvetica-Bold", 10)
-    pdf.drawString((width - paymentSummaryStringWidth)/2, height-(3.2*inch), "Payment Summary")
+    pdf.drawString((width - paymentSummaryStringWidth)/2, height-(heightTracker*inch), "Payment Summary")
 
-    pdf.drawString((inch*1.1), height-(3.4*inch), "Gross Earnings: ")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Gross Earnings: ")
     pdf.setFont("Helvetica", 10)
-    pdf.drawRightString((width-(inch*1.1)), height-(3.4*inch), grossEarnings)
-
-    pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(3.6*inch), "Total Taxes and Deductions: ")
-    pdf.setFont("Helvetica", 10)
-    pdf.drawRightString((width-(inch*1.1)), height-(3.6*inch), totalTaxesAndDeductions)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), grossEarnings)
 
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(3.8*inch), "Net Pay: ")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Total Taxes and Deductions: ")
     pdf.setFont("Helvetica", 10)
-    pdf.drawRightString((width-(inch*1.1)), height-(3.8*inch), netPay)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), totalTaxesAndDeductions)
 
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(4*inch), "Direct Deposit To: ")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Net Pay: ")
     pdf.setFont("Helvetica", 10)
-    pdf.drawRightString((width-(inch*1.1)), height-(4*inch), bankName + "-" + bankMask)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), netPay)
+
+    pdf.setFont("Helvetica-Bold", 10)
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Direct Deposit To: ")
+    pdf.setFont("Helvetica", 10)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), bankName + "-" + bankMask)
 
     # Box Payment Summary Information
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.rect(0.9*inch, height-(4.1*inch), width-(1.8*inch), inch*1.1)
+    heightTracker += 0.1
+    pdf.rect(0.9*inch, height-(heightTracker*inch), width-(1.8*inch), inch*1.1)
 
     # Draw Gross Earnings Section
     pdf.setFont("Helvetica-Bold", 10)
+    heightTracker += 0.2
     grossEarningsSummaryWidth = stringWidth("Gross Earnings", "Helvetica-Bold", 10)
-    pdf.drawString((width - grossEarningsSummaryWidth)/2, height-(4.3*inch), "Gross Earnings")
+    pdf.drawString((width - grossEarningsSummaryWidth)/2, height-(heightTracker*inch), "Gross Earnings")
     
-    pdf.drawString((inch*1.1), height-(4.5*inch), "Description")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Description")
     currentStringWidth = stringWidth("Current", "Helvetica-Bold", 10)
-    pdf.drawString((width-currentStringWidth)/2, height-(4.5*inch), "Current")
-    pdf.drawRightString((width-(inch*1.1)), height-(4.5*inch), "YTD")
+    pdf.drawString((width-currentStringWidth)/2, height-(heightTracker*inch), "Current")
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), "YTD")
 
     pdf.setFont("Helvetica", 10)
-    pdf.drawString((inch*1.1), height-(4.7*inch), "Salary")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Salary")
     currentSalaryWidth = stringWidth(currentSalary, "Helvetica", 10)
-    pdf.drawString((width-currentSalaryWidth)/2, height-(4.7*inch), currentSalary)
-    pdf.drawRightString((width-(inch*1.1)), height-(4.7*inch), ytdSalary)
+    pdf.drawString((width-currentSalaryWidth)/2, height-(heightTracker*inch), currentSalary)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), ytdSalary)
 
-    pdf.drawString((inch*1.1), height-(4.9*inch), "Supplemental pay: Bonus")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Supplemental pay: Bonus")
     currentSupplementalBonusWidth = stringWidth(currentSupplementalBonus, "Helvetica", 10)
-    pdf.drawString((width-currentSupplementalBonusWidth)/2, height-(4.9*inch), currentSupplementalBonus)
-    pdf.drawRightString((width-(inch*1.1)), height-(4.9*inch), ytdSupplementalBonus)
+    pdf.drawString((width-currentSupplementalBonusWidth)/2, height-(heightTracker*inch), currentSupplementalBonus)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), ytdSupplementalBonus)
 
-    pdf.line((inch*1.1), height-(5.0*inch), width-(inch*1.1), height-(5.0*inch))
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch)m "Supplemental Pay: Overtime")
+    currentSupplementalOvertimeWidth = stringWidth(currentOvertime, "Helvetica", 10)
+    pdf.drawString((width-currentSupplementalOvertimeWidth)/2, height-(heightTracker*inch), currentOvertime)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), ytdOvertime)
+
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch)m "Supplemental Pay: Commission")
+    currentCommissionWidth = stringWidth(currentCommission, "Helvetica", 10)
+    pdf.drawString((width-currentCommissionWidth)/2, height-(heightTracker*inch), currentCommission)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), ytdCommission)
+
+    heightTracker += 0.1
+    pdf.line((inch*1.1), height-(heightTracker*inch), width-(inch*1.1), height-(heightTracker*inch))
 
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(5.2*inch), "Gross Earnings Totals")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Gross Earnings Totals")
     currentTotalWidth = stringWidth(currentTotalEarnings, "Helvetica-Bold", 10)
-    pdf.drawString((width-currentTotalWidth)/2, height-(5.2*inch), currentTotalEarnings)
-    pdf.drawRightString((width-(inch*1.1)), height-(5.2*inch), ytdTotalEarnings)
+    pdf.drawString((width-currentTotalWidth)/2, height-(heightTracker*inch), currentTotalEarnings)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), ytdTotalEarnings)
 
     # Box Gross Earnings Section
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.rect(0.9*inch, height-(5.3*inch), width-(1.8*inch), inch*1.2)
+    heightTracker += 0.1
+    pdf.rect(0.9*inch, height-(heightTracker*inch), width-(1.8*inch), inch*1.6)
 
     # Draw Taxes Withheld Section
     pdf.setFont("Helvetica-Bold", 10)
+    heightTracker += 0.2
     taxesWithheldWidth = stringWidth("Taxes Withheld", "Helvetica-Bold", 10)
-    pdf.drawString((width - taxesWithheldWidth)/2, height-(5.5*inch), "Taxes Withheld")
+    pdf.drawString((width - taxesWithheldWidth)/2, height-(heightTracker*inch), "Taxes Withheld")
 
-    pdf.drawString((inch*1.1), height-(5.7*inch), "Description")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Description")
     currentStringWidth = stringWidth("Current", "Helvetica-Bold", 10)
-    pdf.drawString((width-currentStringWidth)/2, height-(5.7*inch), "Current")
-    pdf.drawRightString((width-(inch*1.1)), height-(5.7*inch), "YTD")
+    pdf.drawString((width-currentStringWidth)/2, height-(heightTracker*inch), "Current")
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), "YTD")
 
     pdf.setFont("Helvetica", 10)
-    pdf.drawString((inch*1.1), height-(5.9*inch), "Federal Income Tax")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Federal Income Tax")
     currentFederalIncomeTaxWidth = stringWidth(currentFederalIncomeTax, "Helvetica", 10)
-    pdf.drawString((width-currentFederalIncomeTaxWidth)/2, height-(5.9*inch), currentFederalIncomeTax)
+    pdf.drawString((width-currentFederalIncomeTaxWidth)/2, height-(heightTracker*inch), currentFederalIncomeTax)
 
-    pdf.drawString((inch*1.1), height-(6.1*inch), "Social Security")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Social Security")
     currentSocialSecurityWidth = stringWidth(currentSocialSecurity, "Helvetica", 10)
-    pdf.drawString((width-currentSocialSecurityWidth)/2, height-(6.1*inch), currentSocialSecurity)
+    pdf.drawString((width-currentSocialSecurityWidth)/2, height-(heightTracker*inch), currentSocialSecurity)
 
-    pdf.drawString((inch*1.1), height-(6.3*inch), "Medicare")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Medicare")
     currentMedicareWidth = stringWidth(currentMedicare, "Helvetica", 10)
-    pdf.drawString((width-currentMedicareWidth)/2, height-(6.3*inch), currentMedicare)
+    pdf.drawString((width-currentMedicareWidth)/2, height-(heightTracker*inch), currentMedicare)
 
-    pdf.drawString((inch*1.1), height-(6.5*inch), "State Income Tax")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "State Income Tax")
     currentStateTaxWidth = stringWidth(currentStateTax, "Helvetica", 10)
-    pdf.drawString((width-currentStateTaxWidth)/2, height-(6.5*inch), currentStateTax)
+    pdf.drawString((width-currentStateTaxWidth)/2, height-(heightTracker*inch), currentStateTax)
 
-    pdf.line((inch*1.1), height-(6.6*inch), width-(inch*1.1), height-(6.6*inch))
+    heightTracker += 0.1
+    pdf.line((inch*1.1), height-(heightTracker*inch), width-(inch*1.1), height-(heightTracker*inch))
 
     pdf.setFont("Helvetica-Bold", 10)
-    pdf.drawString((inch*1.1), height-(6.8*inch), "Taxes Withheld Totals")
+    heightTracker += 0.2
+    pdf.drawString((inch*1.1), height-(heightTracker*inch), "Taxes Withheld Totals")
     currentTotalTaxesWidth = stringWidth(currentTotalTaxWithheld, "Helvetica-Bold", 10)
-    pdf.drawString((width-currentTotalTaxesWidth)/2, height-(6.8*inch), currentTotalTaxWithheld)
-    pdf.drawRightString((width-(inch*1.1)), height-(6.8*inch), ytdTotalTaxWithheld)
+    pdf.drawString((width-currentTotalTaxesWidth)/2, height-(heightTracker*inch), currentTotalTaxWithheld)
+    pdf.drawRightString((width-(inch*1.1)), height-(heightTracker*inch), ytdTotalTaxWithheld)
 
     # Box Taxes Withheld Section
     pdf.setFont("Helvetica-Bold", 6)
-    pdf.rect(0.9*inch, height-(6.9*inch), width-(1.8*inch), inch*1.6)
+    heightTracker += 0.1
+    pdf.rect(0.9*inch, height-(heightTracker*inch), width-(1.8*inch), inch*1.6)
 
     pdf.setFillColor(navy)
+    heightTracker += 3.6
     footerMessage = "Information Provided by CaseSwift: The GOAT PI SAAS Platform"
     footerLength = stringWidth(footerMessage, "Helvetica-Bold", 6)
     currentStringWidth = stringWidth("Current", "Helvetica-Bold", 10)
@@ -251,7 +299,7 @@ def send_email(sender, recipient, plaidResponseObject):
         pdfName = "plaid_income_verification" + str(counter) + ".pdf"
         plaidFinancialInformationPdf = canvas.Canvas("/tmp/" + pdfName, pagesize=letter)
         width, height = letter
-        createPdf(plaidFinancialInformationPdf, width, height, item)
+        createPlaidPayrollPdf(plaidFinancialInformationPdf, width, height, item)
         part = MIMEApplication(open("/tmp/"+ pdfName, 'rb').read())
         part.add_header('Content-Disposition', 'attachment', filename=pdfName)
         msg.attach(part)
