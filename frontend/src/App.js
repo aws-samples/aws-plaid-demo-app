f31d44d009a1662ae09e317490204113d6ea9cfe
ericdoppelt <ead45@duke.edu>
2023-08-27 18:22:49 -0400

    Fixed import statements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Authenticator } from "@aws-amplify/ui-react";
 import Protected from './pages/Protected';
 import Login from './pages/Login';
-import Institution from './pages/Institution';
 import Layout from './components/Layout';
 import RequireAuth from './RequireAuth';
 
