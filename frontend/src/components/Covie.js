3b371d6fc3b0237cbba70875b50fe43d024f1e9d
ericdoppelt <ead45@duke.edu>
2023-08-28 19:20:59 -0400

    covie print statement

--- frontend/src/components/Covie.js
+++ frontend/src/components/Covie.js
@@ -5,15 +5,19 @@ import { Button, Flex } from '@aws-amplify/ui-react';
 const logger = new Logger('Covie');
 
 export default function Covie() {
-
   useEffect(() => {
-    window.covieReady = function(){' '}
-        {window.Covie.access.button({
-          elementId: 'covie-root',
-          buttonTheme: 'covie',
-          buttonText: 'Link Insurance',
-          embed: { integrationKey: 'ik_emx55cdek5xtz2ro', linkId: '', metadata: {}, hide: [] },
-        })}
+    window.covieReady = function () {
+      ' ';
+    };
+    {
+      window.Covie.access.button({
+        elementId: 'covie-root',
+        buttonTheme: 'covie',
+        buttonText: 'Link Insurance',
+        embed: { integrationKey: 'ik_emx55cdek5xtz2ro', linkId: '', metadata: {}, hide: [] },
+        onSuccess: (linkId, policies) => { console.log(policies) }
+      });
+    }
   }, []);
 
   return (
