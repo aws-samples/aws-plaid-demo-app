db78f1984ae0e94ccd92c5977a8bbff042bf58ae
ericdoppelt <ead45@duke.edu>
2023-08-28 20:32:13 -0400

    covie

--- frontend/src/components/Covie.js
+++ frontend/src/components/Covie.js
@@ -7,9 +7,24 @@ const logger = new Logger('Covie');
 function printCovie(linkId, policies) {
   console.log(linkId);
   console.log(policies);
+  setState(linkId, policies);
+}
+
+function setState(linkId, policies) {
+  setLinkId(linkId);
+  setPolicyIds(policies);
 }
 
 export default function Covie() {
+  // Get lawyer's email to send data to.
+  const { user } = useAuthenticator((context) => [context.user]);
+  const userEmail = user.signInUserSession.idToken.payload.email;
+
+  // State to track Plaid variables.
+  const [linkId, setLinkId] = useState(null);
+  const [policyIds, setPolicyIds] = useState(null);
+
+  // Establish the link button. This code was provided by Covie.
   useEffect(() => {
     window.covieReady = function () {
       ' ';
@@ -24,10 +39,10 @@ export default function Covie() {
           linkId: '',
           metadata: {},
           hide: [],
-          onSuccess: printCovie
+          onSuccess: printCovie,
         },
       });
-    };
+    }
   }, []);
 
   return (
