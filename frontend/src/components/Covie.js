a1d1b9e908a6b04fe8881a79e53fb494ec4e072e
ericdoppelt <ead45@duke.edu>
2023-08-28 20:37:02 -0400

    covie

--- frontend/src/components/Covie.js
+++ frontend/src/components/Covie.js
@@ -4,17 +4,6 @@ import { Button, Flex } from '@aws-amplify/ui-react';
 
 const logger = new Logger('Covie');
 
-function printCovie(linkId, policies) {
-  console.log(linkId);
-  console.log(policies);
-  setState(linkId, policies);
-}
-
-function setState(linkId, policies) {
-  setLinkId(linkId);
-  setPolicyIds(policies);
-}
-
 export default function Covie() {
   // Get lawyer's email to send data to.
   const { user } = useAuthenticator((context) => [context.user]);
@@ -24,6 +13,17 @@ export default function Covie() {
   const [linkId, setLinkId] = useState(null);
   const [policyIds, setPolicyIds] = useState(null);
 
+  function printCovie(linkId, policies) {
+    console.log(linkId);
+    console.log(policies);
+    setState(linkId, policies);
+  }
+  
+  function setState(linkId, policies) {
+    setLinkId(linkId);
+    setPolicyIds(policies);
+  }
+  
   // Establish the link button. This code was provided by Covie.
   useEffect(() => {
     window.covieReady = function () {
