55aa0918a72f34c36a52dd329518aaa610f5fa3c
ericdoppelt <ead45@duke.edu>
2023-09-25 00:38:59 -0400

    covie fix

--- frontend/src/components/Onboarding/Covie.js
+++ frontend/src/components/Onboarding/Covie.js
@@ -16,29 +16,6 @@ export default function Covie({ setCovieToggle, setCoviePolicies }) {
     setCovieToggle(false);
   };
 
-  const sendPolicyRequest = async () => {
-    try {
-      const res = await API.post(apiName, '/v1/tokens/auto-policy', {
-        body: {
-          policies: policies,
-          email: userEmail,
-        },
-      });
-      logger.debug('POST /v1/tokens/auto-policy response:', res);
-    } catch (err) {
-      logger.error('Unable to get policy info:', err);
-    }
-    setPolicyRequest(false);
-  };
-
-  // Send the policy request once the link is complete.
-  useEffect(() => {
-    if (policyRequest && policies) {
-      sendPolicyRequest();
-      setPolicyRequest(false);
-    }
-  }, [policyRequest, policies]);
-
   // Establish the link button. This code was provided by Covie.
   useEffect(() => {
     window.Covie.access.init({
