392970e9f3d475c7dfe562aad024f40a05ed96a8
Dylan Wallach <22645038+FireWallach@users.noreply.github.com>
2023-09-25 22:33:08 -0400

    auth in local

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "caseswiftfrontendcf7ce39b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,34 +11,44 @@ const root = createRoot(container);
 
 Amplify.Logger.LOG_LEVEL = 'DEBUG';
 
-Amplify.configure({
-  aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_URL,
-  aws_appsync_region: process.env.REACT_APP_REGION,
-  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-  Auth: {
-    region: process.env.REACT_APP_REGION,
-    userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-    mandatorySignIn: true,
-    oauth: {
-      domain: process.env.REACT_APP_COGNITO_DOMAIN,
-      scope: ['email', 'openid', `${process.env.REACT_APP_BACKEND_URL}/plaid.rw}`],
-      responseType: 'code'
-    }
-  },
-  API: {
-    endpoints: [
-      {
-        name: "plaidapi",
-        endpoint: process.env.REACT_APP_BACKEND_URL,
+// Try to dynamically import aws-exports
+import('./aws-exports')
+  .then(awsconfig => {
+    // If aws-exports.js is available
+    Amplify.configure(awsconfig.default);
+  })
+  .catch(err => {
+    // If aws-exports.js is NOT available, use the old settings
+    Amplify.configure({
+      aws_appsync_graphqlEndpoint: process.env.REACT_APP_GRAPHQL_URL,
+      aws_appsync_region: process.env.REACT_APP_REGION,
+      aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+      Auth: {
         region: process.env.REACT_APP_REGION,
-        clientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
-        custom_header: async () => {
-          return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
+        userPoolId: process.env.REACT_APP_COGNTIO_USERPOOL_ID,
+        userPoolWebClientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+        mandatorySignIn: true,
+        oauth: {
+          domain: process.env.REACT_APP_COGNITO_DOMAIN,
+          scope: ['email', 'openid', `${process.env.REACT_APP_BACKEND_URL}/plaid.rw}`],
+          responseType: 'code'
         }
+      },
+      API: {
+        endpoints: [
+          {
+            name: "plaidapi",
+            endpoint: process.env.REACT_APP_BACKEND_URL,
+            region: process.env.REACT_APP_REGION,
+            clientId: process.env.REACT_APP_COGNITO_CLIENT_ID,
+            custom_header: async () => {
+              return { Authorization: `Bearer ${(await Auth.currentSession()).getAccessToken().getJwtToken()}` }
+            }
+          }
+        ]
       }
-    ]
-  }
-});
-
-root.render(<App />);
+    });
+  })
+  .finally(() => {
+    root.render(<App />);
+  });
